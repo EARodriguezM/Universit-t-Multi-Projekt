8c61379441647ad55b3b0d230cf613d3170f9729
EARodriguezM <esarodriguezme@gmail.com>
2020-05-18 18:03:39 -0500

    Delete FrontEnd

--- FrontEnd/projects/shared/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of shared
- */
-
-export * from './lib/components/components.module';
-export * from './lib/components/header/header.component';
-export * from './lib/components/footer/footer.component';
-
-export * from './lib/interceptors/token.interceptor';
